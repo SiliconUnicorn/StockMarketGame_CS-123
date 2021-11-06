44b0e623989ac837a3d9746adf463322cfb66f0d
aabeyse1 <93781833+aabeyse1@users.noreply.github.com>
2021-11-06 13:30:07 -0500

    Update stockMarket_main.py

--- stockMarket/code/stockMarket/stockMarket_main.py
+++ stockMarket/code/stockMarket/stockMarket_main.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def stock(start_price):
+    """Takes in the the starting price of a stock as an input, and generates a randomized graph representing the 
+    stock, starting at the initial price. """
     mu = 0.004
     sigma = 0.01
     np.random.seed(0)
