425d642ad5c49696d7296f2de57dc2dfc8f78c43
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-11-06 14:10:20 -0500

    Allows the program to call the turn updater function thirty times.

--- stockMarket/code/coreCode/main.py
+++ stockMarket/code/coreCode/main.py
@@ -5,12 +5,15 @@ Date: 5 Nov 2021
 Author(s): Micah Hansonbrook
 '''
 
+from turnUpdater import *
+
 def main():
     '''
     The main function of the program
     :return: None
     '''
-    print("No Action")
+    for turn in range(31):
+        turnUpdater(turn)
 
 if __name__ == "__main__":
     main()
