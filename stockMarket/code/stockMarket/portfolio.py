5f02f61b9b9a9c906c605866195330d4dff21938
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-11-21 10:38:35 -0600

    Added the portfolio class.

---
+++ stockMarket/code/stockMarket/portfolio.py
@@ -0,0 +1,12 @@
+'''
+File: portfolio.py
+Project: Stock Market Game
+Date: 21 Nov 2021
+Author(s): Micah Hansonbrook
+'''
+
+class Portfolio:
+    def __init__(self):
+        self.userCash = 100.00
+        self.playerID = "Human Player"
+        # self.ownedStocks = {100: Stock("AAPL")} (Uncomment once stock class exists.
