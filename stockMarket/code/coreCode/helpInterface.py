ea3b3e2919e24a6f4e6e91659a8c83d06942c516
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-12-06 11:19:09 -0500

    Improved phrasing.

--- stockMarket/code/coreCode/helpInterface.py
+++ stockMarket/code/coreCode/helpInterface.py
@@ -24,7 +24,7 @@ class HelpInterface:
         self.scroll = sTk.ScrolledText(self.rootWin, font="Helvetica", wrap=tk.WORD)
         self.scroll.grid(column=1, row=2)
         self.scroll.insert(1.0, """\tIn real life, the stock market allows individuals to trade shares of large corporations. Every quarter, shareholders either receive a portion of corporate profits, or pay a portion of corporate losses.
-        \tIn our game, the user acts as an investor, but dividends are not  paid out. Instead, the user must solely attempt to increase the value of their shares through strategic purchases and sales.
+        \tIn our game, the user acts as an investor, but dividends are not  paid out. Instead, the user must  attempt to increase the value of their shares solely through strategic purchases and sales.
         \tThe end goal of the game is to accrue as much money as is possible. Victory is achieved if the user surpasses one million dollars before 2021.""")
 
         self.button = tk.Button(self.rootWin)
