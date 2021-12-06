95cbf78150808f83063f7f2132f4a5d05ea51bf0
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-12-06 11:11:24 -0500

    Made some changes to the help interface.

--- stockMarket/code/coreCode/helpInterface.py
+++ stockMarket/code/coreCode/helpInterface.py
@@ -21,9 +21,11 @@ class HelpInterface:
         self.mainText.grid(column=1, row=1)
         self.mainText['text'] = "How the Stock Market Works"
 
-        self.scroll = sTk.ScrolledText(self.rootWin)
+        self.scroll = sTk.ScrolledText(self.rootWin, font="Helvetica")
         self.scroll.grid(column=1, row=2)
-        self.scroll.insert(1.0, """\tIn real life, the stock market allows individuals to trade shares of large corporations. Every quarter, shareholders either receive a portion of corporate profits, or pay a portion of corporate losses.""")
+        self.scroll.insert(1.0, """\tIn real life, the stock market allows individuals to trade shares of large corporations. Every quarter, shareholders either receive a portion of corporate profits, or pay a portion of corporate losses.
+        \tIn our game, the user acts as an investor, but dividends are not  paid out. Instead, the user must solely attempt to increase the value of their shares through strategic purchases and sales.
+        \tThe end goal of the game is to accrue as much money as is possible. Victory is achieved if the user surpasses one million dollars before 2021.""")
 
         self.button = tk.Button(self.rootWin)
         self.button.grid(column=1, row=3)
