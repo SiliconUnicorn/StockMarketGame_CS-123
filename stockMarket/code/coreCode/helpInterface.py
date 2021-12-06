286e1eafc461ce5bfebbe6558fab3601fab10691
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-12-06 11:18:25 -0500

    Text now wraps correctly.

--- stockMarket/code/coreCode/helpInterface.py
+++ stockMarket/code/coreCode/helpInterface.py
@@ -21,7 +21,7 @@ class HelpInterface:
         self.mainText.grid(column=1, row=1)
         self.mainText['text'] = "How the Stock Market Works"
 
-        self.scroll = sTk.ScrolledText(self.rootWin, font="Helvetica")
+        self.scroll = sTk.ScrolledText(self.rootWin, font="Helvetica", wrap=tk.WORD)
         self.scroll.grid(column=1, row=2)
         self.scroll.insert(1.0, """\tIn real life, the stock market allows individuals to trade shares of large corporations. Every quarter, shareholders either receive a portion of corporate profits, or pay a portion of corporate losses.
         \tIn our game, the user acts as an investor, but dividends are not  paid out. Instead, the user must solely attempt to increase the value of their shares through strategic purchases and sales.
