e3e23aee3f7af442ca25c6745feb607a28ca0008
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-12-11 11:11:20 -0600

    Added two new dynamic event options.

--- stockMarket/code/events/event.py
+++ stockMarket/code/events/event.py
@@ -20,11 +20,17 @@ class Event:
 
     def generateDynamicEvent(self):
         '''Generates a dynamically generated event, to compliment the hardcoded ones'''
-        randomValue = random.randint(0, 0)
+        randomValue = random.randint(0, 2)
         companies = getStockList()
         finalCompany = companies[random.randint(0, len(companies)-1)]
         if randomValue == 0:
             randomValue = float(random.randint(6,15))/100
-            return Event(finalCompany.name + ' releases a hit new product!', 'Everyone wants to purchase the hit new product from ' + finalCompany.name, finalCompany.name, randomValue)
+            return Event(finalCompany.name + ' Releases Hit New Product!', 'Everyone wants to purchase the hit new product from ' + finalCompany.name, finalCompany.name, randomValue)
+        elif randomValue == 1:
+            randomValue = float(random.randint(9, 19)) / 100
+            return Event(finalCompany.name + ' is Undervalued', 'Investment sources claim that ' + finalCompany.name + ' is severely undervalued.', finalCompany.name, randomValue)
+        elif randomValue == 2:
+            randomValue = float(random.randint(-8, -2)) / 100
+            return Event(finalCompany.name + ' Under Investigation', 'Antitrust authorities are currently investigating ' + finalCompany.name + ' for anticompetitive behavior.', finalCompany.name, randomValue)
         else:
             assert 0 == 1
