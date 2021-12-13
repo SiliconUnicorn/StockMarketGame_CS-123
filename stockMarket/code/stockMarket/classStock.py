e49a2092af5fbff4338a8b4f73ea813dde4d7e0e
aabeyse1 <93781833+aabeyse1@users.noreply.github.com>
2021-12-13 11:02:50 -0600

    Update classStock.py

--- stockMarket/code/stockMarket/classStock.py
+++ stockMarket/code/stockMarket/classStock.py
@@ -2,7 +2,7 @@
 Names: Arnika Abeysekera, Micah Hansonbrook, Sarah Ali, Tina Chen
 Course: COMP123-01
 Instructor: Lauren Milne
-This file contains the Stock class. 
+Description: This file contains the Stock class. 
 """
 
 import random
